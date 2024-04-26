d39e3f0b421c7c2679b91397f4bc3a3ae2f28ce0
Joseph Monticelli <jdmonticelli@gmail.com>
2024-04-26 11:40:42 -0400

    corrected syntax error creating a bug at the end of quiz. all seems to be working now.

--- assets/js/studentData.js
+++ assets/js/studentData.js
@@ -7,18 +7,14 @@ const clearButtonEl = document.getElementById("clear-button");
 const backButtonEl = document.getElementById("back-button");
 
 function storeStudentData(nameInputEl, finalScoreEl, questionSectionEl, resultEl) {
-    // console.log("hello")
     var studentData = {
         name: nameInputEl.value,
         score: finalScoreEl.textContent
     };
-    // studentDatalog.push(studentData);
-    // localStorage.setItem('studentDatalog', JSON.stringify(studentDatalog));
     questionSectionEl.classList.add("hide");
     resultEl.classList.add("hide");
     nameInputEl.classList.add("hide");
     highScoresEl.classList.remove("hide");
-    // viewScoreEl.classList.add("hide");
     timerEl.classList.add("hide");
 
 
